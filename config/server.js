11226ff01603115d50e3a1904540ffd407efce8c
devarshivaidya2307@gmail.com <YOUR EMAIL>
2021-07-18 10:59:04 +0530

    stage changes

--- config/server.js
+++ config/server.js
@@ -1,9 +1,15 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '567f8abfb2819bd8bd1aa706fdb24d9e'),
     },
   },
 });
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const port = 1337;
+
+server.listen({ port });
